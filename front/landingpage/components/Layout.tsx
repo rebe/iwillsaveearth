4d70ac4de10cbb0eee858c01d15263b679b530a8
Pekka Reijula <pekka.reijula@wirepas.com>
2023-05-26 12:55:36 +0300

    Frontend built with TypeScript

---
+++ front/landingpage/components/Layout.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react'
+
+import Head from 'next/head'
+import Link from 'next/link'
+
+type Props = {
+  children?: ReactNode
+  title?: string
+}
+
+const Layout = ({ children, title = 'This is the default title' }: Props) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
+        <Link href="/users">Users List</Link> |{' '}
+        <Link href="/api/users">Users API</Link>
+      </nav>
+    </header>
+    {children}
+    <footer>
+      <hr />
+      <span>I&apos;m here to stay (Footer)</span>
+    </footer>
+  </div>
+)
+
+export default Layout
