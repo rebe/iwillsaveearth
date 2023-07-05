985079d53ca36de1514ed39cce0e0a8ea4318d11
Pekka Reijula <pekka.reijula@wirepas.com>
2023-07-05 17:03:18 +0300

    Nginx ec2 instace for naked domain proxy to s3 bucket

---
+++ cdk/nginx/lib/nginx-stack.ts
@@ -0,0 +1,81 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from 'aws-cdk-lib/aws-iam'
+import * as path from 'path';
+
+// import { KeyPair } from 'cdk-ec2-key-pair';
+import { Asset } from 'aws-cdk-lib/aws-s3-assets';
+import { Construct } from 'constructs';
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class NginxStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'NginxQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    // Create new VPC with 2 Subnets
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      natGateways: 0,
+      subnetConfiguration: [{
+        cidrMask: 24,
+        name: "iwillsaveearth",
+        subnetType: ec2.SubnetType.PUBLIC
+      }]
+    });
+
+    // Allow SSH (TCP Port 22) access from anywhere
+    const securityGroup = new ec2.SecurityGroup(this, 'Nginx SG', {
+      vpc,
+      description: 'Open nginx SG',
+      allowAllOutbound: true
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH Access')
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow SSH Access')
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow SSH Access')
+
+    const role = new iam.Role(this, 'ec2Role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    })
+
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'))
+
+    // Use Latest Amazon Linux Image - CPU Type ARM64
+    const ami = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.ARM_64
+    });
+
+    // Create the instance using the Security Group, AMI, and KeyPair defined in the VPC created
+    const ec2Instance = new ec2.Instance(this, 'NginxInstance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
+      machineImage: ami,
+      securityGroup: securityGroup,
+      keyName: "general_ec2key", // needs to be create in AWS account
+      role: role
+    });
+
+    // Create an asset that will be used as part of User Data to run on first load
+    const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../src/config.sh') });
+    const localPath = ec2Instance.userData.addS3DownloadCommand({
+      bucket: asset.bucket,
+      bucketKey: asset.s3ObjectKey,
+    });
+
+    ec2Instance.userData.addExecuteFileCommand({
+      filePath: localPath,
+      arguments: '--verbose -y'
+    });
+    asset.grantRead(ec2Instance.role);
+
+    // Create outputs for connecting
+    new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
+    new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i key.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp })
+  }
+}
---
+++ cdk/nginx/test/nginx.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Nginx from '../lib/nginx-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nginx-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Nginx.NginxStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
